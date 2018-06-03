80bc90ee3e1f1b96e03d42943369a0e7c7efa3d7
Jozue06 <jozue06@gmail.com>
2018-06-03 16:37:53 -0700

    setup ok, on to tests

--- src/app.js
+++ src/app.js
@@ -1,59 +1,61 @@
 
 'use strict';
 
-// 1st Party library
-const http = require('http');
 const cowsay = require('cowsay');
-
-// Local Libraries
-// parser will tear the URL apart and give us back an object with things like path, query params, etc.
-// it will also deal with POST data and append JSON to req.body if sent
+const fs = require('fs');
+const http = require('http');
 const parser = require('../src/lib/parser.js');
 
 const requestHandler = (req,res) => {
+  let errorPage = (err) => {
 
-  // Take a look here if you're interested to see what some parts of the request object are.
-  // console.log(req.method);
-  // console.log(req.headers);
-  // console.log(req.url);
-
-  // In all cases, parse the URL
+    console.log('error!', err);
+    res.writeHead(500);
+    res.write(err.message);
+    res.end();
+  };
   parser(req)
     .then( req => {
-
-      //The "if" statements below are our "routes" and do the same things that express does (below) but 100% manually
-      // app.get('/', (req,res) => res.send('Hello From the Gutter'));
-      // app.get('/foo', (req,res) => res.send('Hello From the Gutter'));
       
-      if ( req.method === 'GET' && req.url.pathname === '/cowsay' ) {
-        res.setHeader('Content-Type', 'text/html');
-        res.statusCode = 200;
-        res.statusMessage = 'OK';
-
-        // Send out some random HTML (actually, it's not totally random. Note how it includes req.url.query.you ...
-        // That would show whatever you have in the URL after you = (http://localhost:3000?this=that&you=cool
-
-        let message = req.url.query.cowsay;
-
-        // message = 'Hola';
-        // res.cowsay.say(message);
-        res.write(`<!DOCTYPE html><html><body><h1>${message}</h1></body></html>`);
-        // ... Instead of doing manual HTML like that, you could have used the "fs" module to read a file
-        // and "res.write()" the contents of that file.
-
-        res.end();
-        return;
+      if ( req.method === 'GET' && req.url.pathname === '/' ) {
+        fs.readFile('../index.html', (err, data) =>{
+          if(err) { return errorPage();}
+          res.setHeader('Content-Type', 'text/html');
+          res.statusCode = 200;
+          res.statusMessage = 'OK';
+          res.write(data.toString());
+          res.end();
+        });
       }
 
-      // Here, we have a "POST" request which will always return a JSON object.  That object will either be
-      // the JSON that you posted in (just spitting it back out), or an error object, formatted to look like JSON
-      else if ( req.method === 'POST' && req.url.pathname === '/data' ) {
-        res.setHeader('Content-Type', 'text/json');
-        res.statusCode = 200;
-        res.statusMessage = 'OK';
-        res.write( JSON.stringify(req.body) );
-        res.end();
-        return;
+      else if ( req.method === 'GET' && req.url.pathname === '/cowsay' ) {
+        fs.readFile('../cowsay.html', (err, data) =>{
+          if(err) { return errorPage();}
+          let html = data.toString();
+          let text = cowsay.say({text: req.url.query.text});
+          res.setHeader('Content-Type', 'text/html');
+          res.statusCode = 200;
+          res.statusMessage = 'OK';
+          res.write(html.replace('{{cowsay}}',text));
+          res.end();
+        });
+      }
+
+      else if ( req.method === 'POST' && req.url.pathname === '/cowsay' ) {
+        fs.readFile('../cowsay.html', (err, data) => { // *** data might need to be placed on line 49 instead of .text
+          if(err) { return errorPage();}
+          let content = '';
+          if(!req.body) { content = 'Erorr';}
+          else if (req.body.text) {content = cowsay.say({text: req.url.query.text});}
+          else{ content = 'Erorr';}
+
+          let obj = {content: content};
+          res.setHeader('Content-Type', 'text/json');
+          res.statusCode = 200;
+          res.statusMessage = 'OK';
+          res.write( JSON.stringify(obj) );
+          res.end();
+        });       
       }
 
       else {
@@ -64,15 +66,11 @@ const requestHandler = (req,res) => {
         res.end();
       }
 
-    }) // closes the "then" of the parser promise
-    .catch(err => {
-      res.writeHead(500);
-      res.write(err);
-      res.end();
-    });
+    })
+    .catch(errorPage);
 };
 
-// Server callback
+
 const app = http.createServer(requestHandler);
 
 // Expose the start and stop methods.  index.js will call on these.
--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,6 +1,4 @@
 'use strict';
-
-// First Party Modules
 const url = require('url');
 const queryString = require('querystring');
 
@@ -8,15 +6,11 @@ module.exports = (req) => {
 
   return new Promise( (resolve,reject) => {
 
-    if( !(req || req.url) ) { reject('Invalid Request Object. Cannot Parse'); }
-
-    console.log('pre', req.url);
+    if( !(req || req.url) ) { reject('Invalid Request Object. Cannot Parse!!'); }
+    
     req.url = url.parse(req.url);
-    console.log('post', req.url);
 
     req.url.query = queryString.parse(req.url.query);
-
-    console.log('url.query', req.url.query);
     
     if(! req.method.match(/POST|PUT|PATCH/) ) {
       resolve(req);
