b8acf7108543d1f564ae7f69c51d3eda67e888c4
Joshua McClung <jmcclung@genscoinc.corp>
2018-06-05 11:57:30 -0700

    try for heroku

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT;
 
 const ENV = {};
 
-ENV.isProduction = window.location.protocol.includes('the-spot-sea');
+ENV.isProduction = window.location.protocol.includes('josh-cowsay');
 console.log('ENV.isProduction', ENV.isProduction);
 
-ENV.productionApiUrl = 'https://the-spot-sea.herokuapp.com';
+ENV.productionApiUrl = 'https://josh-cowsay.herokuapp.com';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 console.log('apiURL', ENV.apiUrl);
