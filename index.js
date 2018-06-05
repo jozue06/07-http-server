bfebeb9ed869c9fd83d19a72d8fcde2ce7d632b6
Joshua McClung <jmcclung@genscoinc.corp>
2018-06-05 11:56:31 -0700

    try for heroku

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 const dotenv = require('dotenv').config();
 const server = require('./src/app.js');
-server.start( process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
+const PORT = process.env.PORT;
+
+const ENV = {};
+
+ENV.isProduction = window.location.protocol.includes('the-spot-sea');
+console.log('ENV.isProduction', ENV.isProduction);
+
+ENV.productionApiUrl = 'https://the-spot-sea.herokuapp.com';
+ENV.developmentApiUrl = 'http://localhost:3000';
+ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+console.log('apiURL', ENV.apiUrl);
+
+server.start( PORT, () => console.log(`Server up on ${PORT}`));
