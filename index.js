bea381443d7e13c11a8cef565768dccc37a27ff2
Jozue06 <jozue06@gmail.com>
2018-06-05 13:26:51 -0700

    heroku?

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 'use strict';
-// const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config();
 const server = require('./src/app.js');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const ENV = {};
 
-ENV.isProduction = window.location.protocol.includes('josh-cowsay');
-console.log('ENV.isProduction', ENV.isProduction);
-
 ENV.productionApiUrl = 'https://josh-cowsay.herokuapp.com';
-ENV.developmentApiUrl = 'http://localhost:3000';
-ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
-console.log('apiURL', ENV.apiUrl);
+
+
 
 server.start( PORT, () => console.log(`Server up on ${PORT}`));
